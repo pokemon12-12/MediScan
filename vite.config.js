439edabd8168deb149f4eace82bd17d587223aeb
pokemon12-12 <patelanishh1212@gmail.com>
2026-04-07 15:12:54 +0530

    Latest01

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,11 @@
-// import { defineConfig } from 'vite'
-// import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-// import babel from '@rolldown/plugin-babel'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [
-//     react(),
-//     babel({ presets: [reactCompilerPreset()] })
-//   ],
-// })
-
-
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react, { reactCompilerPreset } from '@vitejs/plugin-react'
+import babel from '@rolldown/plugin-babel'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/MediScan/", 
+  plugins: [
+    react(),
+    babel({ presets: [reactCompilerPreset()] })
+  ],
 })
